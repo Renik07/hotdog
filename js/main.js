9129414d5b8b1f41cafeb2ba7ccb391862aabff9
Renat <54742054+Renik07@users.noreply.github.com>
2020-05-05 17:06:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    },
+});
+
+new WOW().init();
